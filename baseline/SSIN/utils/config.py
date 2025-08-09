5011814aa086ac4277183329d601ca9f0dc74704
xren451 <98369049+xren451@users.noreply.github.com>
2025-08-10 03:07:59 +0800

    Delete baseline directory

--- baseline/SSIN/utils/config.py
+++
@@ -1,9 +0,0 @@
-# Configurations ----------------------------------------------------------------- #
-
-# Model params
-d_feat = 1
-d_pos = 2
-scale_emb = True
-
-# Trainer params
-save_train_loss = 100
